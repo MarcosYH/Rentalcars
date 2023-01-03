110beb359e40cf473733105c133637035cf55d0f
MarcosYH <marcosdeimos18@gmail.com>
2023-01-03 19:00:29 +0100

    mise en place logo dans drawer

--- lib/page_accueil.dart
+++ lib/page_accueil.dart
@@ -32,6 +32,39 @@ class _accueilState extends State<accueil> {
         child: Container(
           child: Column(
             children: [
+              Container(
+                color: Colors.blueGrey,
+                width: double.infinity,
+                height: 200,
+                padding: EdgeInsets.only(top: 20.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                     //margin: EdgeInsets.only(bottom: 10),
+                     height: 100,
+                      decoration: BoxDecoration(
+                        //shape: BoxShape.circle,
+                        image: DecorationImage(
+                          image: AssetImage('assets/images/logo.png'),
+                        ),
+                      ),
+                    ),
+                    Text(
+                      "RENTALCARS",
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                    Text(
+                      "Rentalcars@gmail.com",
+                      style: TextStyle(
+                        color: Colors.grey[200],
+                        fontSize: 14,
+                      ),
+                    ),
+                  ],
+                ),
+
+              )
 
             ],
           ),
